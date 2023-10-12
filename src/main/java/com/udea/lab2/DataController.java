c27a5f1f4eb5b029c9a8a915805fd057b6f893fc
Veritax0 <juan.verao@udea.edu.co>
2023-10-11 23:06:32 -0500

    Prueba JaCoCo

--- src/main/java/com/udea/lab2/DataController.java
+++ src/main/java/com/udea/lab2/DataController.java
@@ -46,6 +46,7 @@ public class DataController {
         var objectMapper = new ObjectMapper();
         var faker = new Faker(new Locale("en-US"));
         var currencies = objectMapper.createArrayNode();
+        System.out.println("Holi");
         for(var i=0;i<20;i++){
             var currency = faker.currency();
             currencies.add(objectMapper.createObjectNode()
